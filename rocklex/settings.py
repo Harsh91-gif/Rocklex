06d847598827d7460e5b87cb1a4079f19786f355
Harsh Singh <harshsingh790510@gmail.com>
2026-04-06 09:34:49 +0530

    settings

--- rocklex/settings.py
+++ rocklex/settings.py
@@ -128,5 +128,10 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'harshsingh790510@gmail.com'
-EMAIL_HOST_PASSWORD = 'fzsnnqjijpjqwcfx'
+# EMAIL_HOST_USER = 'harshsingh790510@gmail.com'
+# EMAIL_HOST_PASSWORD = 'fzsnnqjijpjqwcfx'
+
+import os
+
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
